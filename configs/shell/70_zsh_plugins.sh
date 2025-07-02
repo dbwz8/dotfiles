5e420e2defedf35ee8b968159b8da8efa013a457
Bas Nijholt <bas@nijho.lt>
2025-07-02 15:44:46 -0700

    Remove some more defaults

--- configs/shell/70_zsh_plugins.sh
+++ configs/shell/70_zsh_plugins.sh
@@ -3,9 +3,9 @@
 if [[ ($- == *i*) && -n "$ZSH_VERSION" ]]; then
     # -- oh-my-zsh
     [[ -z $STARSHIP_SHELL ]] && export ZSH_THEME="mytheme"
-    DEFAULT_USER="basnijholt"
+    DEFAULT_USER="wecker"
     export DISABLE_AUTO_UPDATE=true  # Speedup of 40%
-    plugins=( git dirhistory history sudo iterm2 uv docker-compose )
+    plugins=( git dirhistory history sudo uv )
     command -v eza >/dev/null && zstyle ':omz:lib:directories' aliases no  # Skip aliases in directories.zsh if eza
     export ZSH=~/dotfiles/submodules/oh-my-zsh
     source $ZSH/oh-my-zsh.sh
