6e7718906746e74bc7bcd049de13a18a69aecfab
wecker <davewecker@outlook.com>
2025-07-07 14:34:19 -0700

    .

--- configs/shell/15_exports.sh
+++ configs/shell/15_exports.sh
@@ -2,6 +2,10 @@
 
 export SAP=~/git/sap
 
+export PATH="$HOME/.local/bin:$PATH"  # Common place, e.g., my upload-file script
+export PATH="/nix/var/nix/profiles/default/bin:$PATH"  # nix path
+export PYTHONPATH=$SAP/qiskit-ionq:$QCIRCUITSIM:$CYPRESS
+
 export CYPRESS=$SAP/system_performance/third_party/cypress/cypress/src
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
@@ -11,9 +15,6 @@ export GITHUB_TOKEN=$(gh auth token)
 export QCIRCUITSIM=$SAP/system_performance/qcircuitsim
 export TMPDIR=/tmp # https://github.com/dotnet/runtime/issues/3168#issuecomment-389070397
 export UPLOAD_FILE_TO="transfer.sh"  # For upload-file.sh
-export PATH="$HOME/.local/bin:$PATH"  # Common place, e.g., my upload-file script
-export PATH="/nix/var/nix/profiles/default/bin:$PATH"  # nix path
-export PYTHONPATH=$SAP/qiskit-ionq:$QCIRCUITSIM:$CYPRESS
 export SYSTEMD_EDITOR=vim
 
 
