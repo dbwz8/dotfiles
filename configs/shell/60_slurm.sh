61b9d2a6dd3324ca042487f4b352b4bb975af0c9
wecker <davewecker@outlook.com>
2025-07-09 20:46:54 +0000

    .

--- configs/shell/60_slurm.sh
+++ configs/shell/60_slurm.sh
@@ -7,15 +7,17 @@ if [[ $- == *i* ]] && command -v squeue &> /dev/null; then
     export TMPDIR=~/.tmp/  # for VScode ssh tmp files
 
     function sq {
-        /usr/bin/squeue -o "que:%9i%10M%3D%2t %5u %15N%Z" $@
+        /usr/bin/squeue -o "que:%i %10M%3D%2t %5u %15N%Z" $@
     }
+
     function sacct {
-        /usr/bin/sacct --format "JobID%5,AllocCPUS%5,State%6,CPUTimeRaw,Elapsed,NTasks%4,NodeList%20,WorkDir%41" $@
+        /usr/bin/sacct --format "JobID,AllocCPUS%4,State%6,CPUTimeRaw,Elapsed,NTasks%4,NodeList%20,WorkDir%21" $@ | head -1
+        /usr/bin/sacct --format "JobID,AllocCPUS%4,State%6,CPUTimeRaw,Elapsed,NTasks%4,NodeList%20,WorkDir%21" $@ | tail -9
     }
 
     #######################################
-    # Show SLURM queue status for Aiida jobs
-    # ARGUMENTS: [-u user] [delaySeconds=10] [aipqeo=aiqeo]
+    # Show SLURM queue status
+    # ARGUMENTS: [-u user] [aipqeo=aiqeo]
     # OUTPUTS:   Repeating queue status
     # HELP:      Give an illegal option
     #######################################
@@ -23,22 +25,19 @@ if [[ $- == *i* ]] && command -v squeue &> /dev/null; then
         local OPTIND option user dir fil opts tim
         user=$USER
         opts="aiqeo"
-        tim=10
         dir="."
         OPTIND=1
-        while getopts ":aiqeot:u:" option; do
+        while getopts ":aiqeou:" option; do
             case "${option}" in
             a) opts=${opts/a/} ;;
             i) opts=${opts/i/} ;;
             q) opts=${opts/q/} ;;
             e) opts=${opts/e/} ;;
             o) opts=${opts/o/} ;;
-            t) tim=$OPTARG ;;
             u) user=$OPTARG ;;
             \?)
                 echo ""
                 echo "Usage: q [-t secs] [-u user] [-aiqeo]"
-                echo "  -t = delay in secs between reports (default=10)"
                 echo "  -u = user to show (default=current user)"
                 echo "  -* = turn off reporting that option:"
                 echo "        a=acct i=info q=queue o=out e=err"
@@ -54,46 +53,36 @@ if [[ $- == *i* ]] && command -v squeue &> /dev/null; then
         shift $(($OPTIND - 1))
         if [[ $opts =~ a ]]; then
             echo "=========================================== acct ===================================================="
-            sacct -u $user --format "JobID%10,AllocCPUS%5,State,CPUTimeRaw,Elapsed,NTasks,NodeList%20,WorkDir%41" | tail -9
+            sacct -u $user
         fi
         if [[ $opts =~ [iqeo] ]]; then
-            local prvMod=$(($(date --utc +%s) - 600))
-            pat1=' (R|CF) '
-                echo "====================================== squeue ==================================================="
+            local prvMod=$(($(date --utc +%s) - 6000))
+            pat1='que:(\S+) .* (R|CF) '
+            echo "====================================== squeue ==================================================="
             sq -hu $user
+            echo "====================================== ==================================================="
             while [ 1 ]; do
-                echo "================================================================================================="
                 [[ $opts =~ i ]] && sinfo -h | grep -e '#' | sed 's/^/inf:           /'
                 [[ $opts =~ q ]] && {
-                    lines=("${(@f)$(sq -hu $user)}")
-                    [[ -z $lines ]] && break
-                    for line in $lines; do
-                        [[ ! $line =~ $pat1 ]] && continue
-                        dir=/home/$user/logs
-                        [[ $opts =~ o ]] && {
-                            fil=$(ls -t $dir/*.stdout 2>/dev/null | head -1)
-                            [[ -s "$fil" ]] && {
-                                local curModOut=$(date --utc --reference=$fil +%s)
+                    for typ in out err;do
+                        [[ $opts =~ ${typ:0:1} ]] && {
+                            files=(/home/$user/logs/*.$typ)
+                            for file in $files; do
+                                local curModOut=$(date --utc --reference=$file +%s)
                                 [[ $curModOut -gt $prvMod ]] && {
-                                    echo "=== fil: $fil"
-                                    tail -10 $fil | sed 's/^/   out:/'
-                                }
-                            }
-                            [[ $opts =~ e ]] && {
-                                fil=$(ls -t $dir/*.stderr 2>/dev/null | head -1)
-                                [[ -s "$fil" ]] && {
-                                    local curModErr=$(date --utc --reference=$fil +%s)
-                                    [[ $curModErr -gt $prvMod ]] && {
-                                        echo "=== fil: $fil"
-                                        tail -10 $fil | sed 's/^/   out:/'
+                                    lines=$(tail -10 $file)
+                                    [[ ! -z "$lines" ]] && {
+                                        echo "=== fil: $file\n$lines"
                                     }
                                 }
-                            }
+                            done
                         }
                     done
+                    lines=("${(@f)$(sq -hu $user)}")
+                    [[ -z $lines ]] && break
                     prvMod=$(($(date --utc +%s) - 2))
                 }
-                sleep $tim
+                sleep 5
             done
         fi
     }
