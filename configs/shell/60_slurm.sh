fea2f33b4ee73caae62922410da2519fbb77fea2
wecker <davewecker@outlook.com>
2025-09-12 14:27:38 -0700

    .

--- configs/shell/60_slurm.sh
+++ configs/shell/60_slurm.sh
@@ -89,7 +89,7 @@ if [[ $- == *i* ]] && command -v squeue &> /dev/null; then
             prvMod=$(($(date --utc +%s) - 2))
             sleep 5
             keepAlive+=5
-            [ $keepAlive -gt 10 ] && {
+            [ $keepAlive -gt 60 ] && {
                 squeue -u $user
                 keepAlive=0
             }
