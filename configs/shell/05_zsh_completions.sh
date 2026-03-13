1a6b977236fe9f9c0a143d37c56ff8c7d2a590d9
wecker <davewecker@outlook.com>
2026-03-13 08:08:22 -0700

    missing completions

--- configs/shell/05_zsh_completions.sh
+++ configs/shell/05_zsh_completions.sh
@@ -2,6 +2,8 @@
 # This needs to run early in the startup process
 
 if [[ -n "$ZSH_VERSION" ]]; then
+    typeset -i rebuild_compdump=0
+
     # Work around broken vendor completion symlinks (seen with Docker Desktop on WSL).
     # A broken entry like /usr/share/zsh/vendor-completions/_docker makes compinit fail.
     vendor_completions_dir=/usr/share/zsh/vendor-completions
@@ -9,6 +11,7 @@ if [[ -n "$ZSH_VERSION" ]]; then
         for completion_file in "$vendor_completions_dir"/_*(N); do
             if [[ -L "$completion_file" && ! -r "$completion_file" ]]; then
                 fpath=(${fpath:#$vendor_completions_dir})
+                rebuild_compdump=1
                 break
             fi
         done
@@ -20,9 +23,16 @@ if [[ -n "$ZSH_VERSION" ]]; then
     # https://gist.github.com/ctechols/ca1035271ad134841284?permalink_comment_id=3994613
     autoload -Uz compinit
     for dump in ~/.zcompdump(N.mh+24); do
-        compinit
+        rebuild_compdump=1
     done
-    compinit -C
+    if [[ ! -f ~/.zcompdump ]]; then
+        rebuild_compdump=1
+    fi
+    if (( rebuild_compdump )); then
+        compinit
+    else
+        compinit -C
+    fi
 
     # Initialize Bash compatibility for completions
     autoload -U +X bashcompinit && bashcompinit
