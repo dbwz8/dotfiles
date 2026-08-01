f794959847e260c6ba84cf042114facdbec95d78
wecker <davewecker@outlook.com>
2026-08-01 08:12:21 -0700

    startup fix

--- configs/shell/65_VcXsrv.sh
+++ configs/shell/65_VcXsrv.sh
@@ -3,8 +3,7 @@ if [ -d /mnt/wslg/runtime-dir ]; then
     export DISPLAY=:0
     unset WAYLAND_DISPLAY
 
-    if command -v dbus-update-activation-environment >/dev/null 2>&1; then
+    if [ -S "/run/user/$(id -u)/bus" ] && command -v dbus-update-activation-environment >/dev/null 2>&1; then
         dbus-update-activation-environment --systemd DISPLAY
     fi
 fi
-
