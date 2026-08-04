329ca16a332e707c5ad81eca74e9a447c8f515cb
wecker <davewecker@outlook.com>
2026-08-04 10:40:24 -0700

    Updated status color

--- configs/nvim/lua/custom/plugins/colorscheme.lua
+++ configs/nvim/lua/custom/plugins/colorscheme.lua
@@ -8,7 +8,8 @@ require('tokyonight').setup {
     colors.bg_float = '#000000'
     colors.bg_popup = '#000000'
     colors.bg_sidebar = '#000000'
-    colors.bg_statusline = '#000000'
+    colors.bg_statusline = '#284b32'
+    colors.bg_statusline_active = '#315b3d'
   end,
   on_highlights = function(highlights, colors)
     highlights.Normal = { bg = colors.bg }
@@ -17,6 +18,11 @@ require('tokyonight').setup {
     highlights.SignColumn = { bg = colors.bg }
     highlights.EndOfBuffer = { bg = colors.bg }
     highlights.FoldColumn = { bg = colors.bg }
+    highlights.MiniStatuslineDevinfo = { fg = colors.fg, bg = colors.bg_statusline_active }
+    highlights.MiniStatuslineFilename = { fg = colors.fg, bg = colors.bg_statusline }
+    highlights.MiniStatuslineFileinfo = { fg = colors.fg, bg = colors.bg_statusline_active }
+    highlights.MiniStatuslineInactive = { fg = colors.green, bg = colors.bg_statusline }
+    highlights.MiniStatuslineModeNormal = { fg = colors.black, bg = colors.green, bold = true }
   end,
 }
 
