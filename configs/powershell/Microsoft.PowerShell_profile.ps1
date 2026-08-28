e29f1e8bb68d14efaffc8970d69e1cb2d0881126
wecker <davewecker@outlook.com>
2026-08-28 07:07:30 -0700

    Revert "Putting back atuin"
    This reverts commit 14b823bd342e055d42d4728cf91f50516b5d24aa.

--- configs/powershell/Microsoft.PowerShell_profile.ps1
+++ configs/powershell/Microsoft.PowerShell_profile.ps1
@@ -144,8 +144,7 @@ if ($PSVersionTable.PSVersion.Major -ge 7) {
     if (Get-Command direnv -ErrorAction SilentlyContinue) {
         Invoke-Expression (& { (direnv hook pwsh | Out-String) })
     }
-    # Set DOTFILES_ENABLE_ATUIN_POWERSHELL=0 to disable Atuin integration.
-    if ($env:DOTFILES_ENABLE_ATUIN_POWERSHELL -ne "0" -and (Get-Command atuin -ErrorAction SilentlyContinue)) {
+    if (Get-Command atuin -ErrorAction SilentlyContinue) {
         Invoke-Expression (& { (atuin init powershell --disable-up-arrow | Out-String) })
     }
 }
